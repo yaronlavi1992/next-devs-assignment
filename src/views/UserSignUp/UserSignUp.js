1079cf33351edb4fa545ec95f79faddc3f694b43
yawikflame <30662244+yawikflame@users.noreply.github.com>
2021-07-16 23:13:52 +0300

    added UserSignUp file added sign-up route using the admin layout added proxy in package.json to the server

---
+++ src/views/UserSignUp/UserSignUp.js
@@ -0,0 +1,241 @@
+import React from 'react';
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+// core components
+import GridItem from 'components/Grid/GridItem.js';
+import GridContainer from 'components/Grid/GridContainer.js';
+import CustomInput from 'components/CustomInput/CustomInput.js';
+import Button from 'components/CustomButtons/Button.js';
+import Card from 'components/Card/Card.js';
+import CardHeader from 'components/Card/CardHeader.js';
+import CardAvatar from 'components/Card/CardAvatar.js';
+import CardBody from 'components/Card/CardBody.js';
+import CardFooter from 'components/Card/CardFooter.js';
+
+import avatar from 'assets/img/faces/marc.jpg';
+
+const styles = {
+  cardCategoryWhite: {
+    color: 'rgba(255,255,255,.62)',
+    margin: '0',
+    fontSize: '14px',
+    marginTop: '0',
+    marginBottom: '0',
+  },
+  cardTitleWhite: {
+    color: '#FFFFFF',
+    marginTop: '0px',
+    minHeight: 'auto',
+    fontWeight: '300',
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: '3px',
+    textDecoration: 'none',
+  },
+};
+
+const useStyles = makeStyles(styles);
+
+export default function UserSignUp() {
+  const classes = useStyles();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const form = document.querySelector('#userSignUpForm');
+    const username = form.username.value;
+    const password = form.password.value;
+    const email = form.emailAddress.value;
+    const firstName = form.firstName.value;
+    const lastName = form.lastName.value;
+    const city = form.city.value;
+    const country = form.country.value;
+    const postalCode = form.postalCode.value;
+    const aboutMe = form.aboutMe.value;
+
+    try {
+      const res = await fetch('/signup', {
+        method: 'POST',
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+          firstName,
+          lastName,
+          city,
+          country,
+          postalCode,
+          aboutMe,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data = await res.json();
+      if (data.errors) {
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={8}>
+          <Card>
+            <form id='userSignUpForm' onSubmit={(e) => onSubmitHandler(e)}>
+              <CardHeader color='primary'>
+                <h4 className={classes.cardTitleWhite}>Sign Up Form</h4>
+                <p className={classes.cardCategoryWhite}>Enter your details</p>
+              </CardHeader>
+              <CardBody>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      labelText='Company (disabled)'
+                      id='company-disabled'
+                      name='company'
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                      inputProps={{
+                        disabled: true,
+                      }}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      labelText='Username'
+                      id='username'
+                      name='username'
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      labelText='Password'
+                      id='password'
+                      name='password'
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                    />
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      labelText='Email address'
+                      id='email-address'
+                      name='emailAddress'
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      labelText='First Name'
+                      id='first-name'
+                      name='firstName'
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      labelText='Last Name'
+                      id='last-name'
+                      name='lastName'
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                    />
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      labelText='City'
+                      id='city'
+                      name='city'
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      labelText='Country'
+                      id='country'
+                      name='country'
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      labelText='Postal Code'
+                      id='postal-code'
+                      name='postalCode'
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                    />
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={12}>
+                    <InputLabel style={{ color: '#AAAAAA' }}>
+                      About me
+                    </InputLabel>
+                    <CustomInput
+                      labelText="Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
+                      id='about-me'
+                      name='aboutMe'
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                      inputProps={{
+                        multiline: true,
+                        rows: 5,
+                      }}
+                    />
+                  </GridItem>
+                </GridContainer>
+              </CardBody>
+              <CardFooter>
+                <Button type='submit' color='primary'>
+                  Sign Up
+                </Button>
+              </CardFooter>
+            </form>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card profile>
+            <CardAvatar profile>
+              <a href='#pablo' onClick={(e) => e.preventDefault()}>
+                <img src={avatar} alt='...' />
+              </a>
+            </CardAvatar>
+            <CardBody profile>
+              <h6 className={classes.cardCategory}>CEO / CO-FOUNDER</h6>
+              <h4 className={classes.cardTitle}>Alec Thompson</h4>
+              <p className={classes.description}>
+                Don{"'"}t be scared of the truth because we need to restart the
+                human foundation in truth And I love you like Kanye loves Kanye
+                I love Rick Owens’ bed design but the back is...
+              </p>
+              <Button color='primary' round>
+                Follow
+              </Button>
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
+}
